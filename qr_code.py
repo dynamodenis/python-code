35c7a1a311480812bc93c346f71e454e0b0b44ec
dynamodenis <dmbugua66@gmail.com>
2020-07-23 20:38:55 +0300

    qr code pip install

--- qr_code.py
+++ qr_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.6
 
-import pyqrcode
-import png
+import pyqrcode # pip install pyqrcode
+import png #pip install pypng
 
 from pyqrcode import QRCode
 QRstr= "https://dynamodenis.herokuapp.com/" # You can change to your prefered url
